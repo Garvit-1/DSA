05c20b7ff4fe606a4209be8be5fa0c3192b3f127
Garvit-1 <117655352+Garvit-1@users.noreply.github.com>
2023-08-03 16:00:05 +0530

    Time: 11 ms (9.12%) | Memory: 11 MB (53.90%) - LeetSync

---
+++ 241-different-ways-to-add-parentheses/different-ways-to-add-parentheses.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+
+int fun(int x,int y,char ch){
+    if(ch=='+') return x+y;
+    else if(ch=='-') return x-y;
+    else if(ch=='*') return x*y;
+    return 0;
+}
+    vector<int> diffWaysToCompute(string ex) {
+       vector<int> ans;
+       int n=ex.length();
+    //    int temp=0;
+    //    if(ex.length()==1){
+    //        ans.push_back()
+    //    } 
+        for(int i=0;i<n;i++){
+            if(ex[i]=='+' || ex[i]=='-' ||ex[i]=='*'){
+                cout<<ex.substr(0,i) <<" "<<ex.substr(i+1,n-i);
+                vector<int> left=diffWaysToCompute(ex.substr(0,i));
+                vector<int> right=diffWaysToCompute(ex.substr(i+1,n-i));
+            for(auto x:left){
+                for(auto y:right){
+                    ans.push_back(fun(x,y,ex[i]));
+                }
+            }
+            }
+        }
+
+        if(ans.size()==0) ans.push_back(stoi(ex));
+        return ans;
+
+
+    
+
+    }
+};
