3fc32703e1acc3835e588cd5d1edf80a8505c801
Garvit-1 <117655352+Garvit-1@users.noreply.github.com>
2023-08-04 21:41:49 +0530

    Time: 17 ms (45.57%) | Memory: 14.9 MB (16.87%) - LeetSync

---
+++ 139-word-break/word-break.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+     unordered_map<string,bool>dp;
+    bool solve(string s,unordered_set<string>&m){
+        if(s.length()==0){
+            return true;
+        }
+        if(dp.find(s)!=dp.end())
+        return dp[s];
+        for(int i=0;i<s.length();i++){
+            string r=s.substr(0,i+1);
+            if(m.count(r)){
+                if(solve(s.substr(i+1),m))
+                return dp[s]=true;
+            }
+        }
+        return dp[s]=false;
+    }
+    bool wordBreak(string s, vector<string>& wordDict) {
+         unordered_set<string>m;
+        for(auto x:wordDict){
+            m.insert(x);
+        }
+         return solve(s,m);
+    }
+};
+
+
+
+////WRONG for Some cases.
+// class Solution {
+// public:
+//     bool wordBreak(string s, vector<string>& wordDict) {
+//         string temp="";
+//         unordered_map<string,int> mp;
+//         for(auto a:wordDict){
+//             mp[a]++;
+//         }
+
+//         for(int i=0;i<s.length();i++){
+//             temp+=s[i];
+//             if(mp[temp]==1){
+//                 temp="";
+//             }
+//         }
+//         if(temp=="") return true;
+        
+//         return false;
+//     }
+// };
