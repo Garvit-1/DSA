fc01f7e59c75cef9523e83d67e5491f109aeb94a
Garvit-1 <117655352+Garvit-1@users.noreply.github.com>
2023-08-09 22:10:39 +0530

    Time: 120 ms (98.99%) | Memory: 81.3 MB (15.35%) - LeetSync

---
+++ 2720-minimize-the-maximum-difference-of-pairs/minimize-the-maximum-difference-of-pairs.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int minimizeMax(vector<int>& v, int p) {
+       sort(v.begin(),v.end());
+       int n = v.size();
+       int ans = v[n-1]-v[0];//Max difference between 2 elements
+       int i=0,j=ans;
+       while(i<=j){
+           int m = (i+j)/2;
+           int total = 0;
+           int k = 0;
+           while(k < n-1){
+               if(v[k+1]-v[k]<=m){    //searching all consecutive enteries
+                   total++;
+                   k+=2;
+               }
+               else k++;
+           }
+           if(total>=p){ // if i have more pairs than required i will restrict 
+               ans = m;
+               j = m-1;
+           }
+           else i = m+1;
+       }
+       return ans;
+    }
+};
