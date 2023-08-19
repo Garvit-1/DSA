a45aa576fe51765a129251489805f3a2dd6a6ad9
Garvit-1 <117655352+Garvit-1@users.noreply.github.com>
2023-08-19 12:25:33 +0530

    Time: 4 ms (83.88%) | Memory: 9.1 MB (6.66%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
-    int minimumTotal(vector<vector<int>>& triangle) {
-        vector<vector<int>> dp(triangle.size(), vector<int>(triangle[triangle.size() - 1].size(), -1));
-        return help(triangle, 0, 0, dp);
-    }
-
-    int help(vector<vector<int>>& triangle, int row, int col, vector<vector<int>>& dp) {
-        if (dp[row][col] != -1) {
-            return dp[row][col];
-        }
-
-        // Base case: If we are at the bottom row, return the value in the triangle.
-        if (row == triangle.size() - 1) {
+int ans=INT_MAX;
+    int help(vector<vector<int>>& triangle,int row,int col,vector<vector<int>>& dp){
+        if(dp[row][col]!=-1) return dp[row][col];
+        
+        if(row==triangle.size()-1) {
             return dp[row][col] = triangle[row][col];
         }
 
-        // Calculate the minimum path sum recursively.
-        int left = help(triangle, row + 1, col, dp);
-        int right = help(triangle, row + 1, col + 1, dp);
+        int left=help(triangle,row+1,col,dp);
+        int right=help(triangle,row+1,col+1,dp);
 
-        // Store the result in the memoization table.
-        return dp[row][col] = triangle[row][col] + min(left, right);
+        dp[row][col]=triangle[row][col]+min(left,right);
+
+        return dp[row][col];
+    }
+    int minimumTotal(vector<vector<int>>& triangle) {
+        vector<vector<int>> dp(triangle.size(), vector<int>(triangle[triangle.size() - 1].size(), -1));
+        return help(triangle,0,0,dp);
+        
     }
 };
