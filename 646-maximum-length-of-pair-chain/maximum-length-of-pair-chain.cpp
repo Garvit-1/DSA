af596b8ab17fb2ed3919a13720874a9210908f0c
Garvit-1 <117655352+Garvit-1@users.noreply.github.com>
2023-08-20 01:13:29 +0530

    Time: 125 ms (69.08%) | Memory: 24.5 MB (35.23%) - LeetSync

---
+++ 646-maximum-length-of-pair-chain/maximum-length-of-pair-chain.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+static bool comp(vector<int>& a,vector<int>& b){
+    return a[1]<b[1];
+}
+    int findLongestChain(vector<vector<int>>& pairs) {
+        sort(pairs.begin(),pairs.end(),comp);
+        int ans=0;
+        for(auto a:pairs) cout<<a[0]<<" "<<a[1];
+        for(int i=0;i<pairs.size();i++){
+            int cure=pairs[i][1];
+            int cur=1;
+            for(int j=i+1;j<pairs.size();j++){
+                if(cure<pairs[j][0]){
+                    cur++;
+                    cure=pairs[j][1];
+                }
+            }
+            if(cur>ans) ans=cur;
+        }
+        return ans;
+    }
+};
