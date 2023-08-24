59241eb9b9a11e7396170152b701132afc87e327
Garvit-1 <117655352+Garvit-1@users.noreply.github.com>
2023-08-24 22:06:19 +0530

    Time: 0 ms (100.00%) | Memory: 6.1 MB (13.81%) - LeetSync

--- 172-factorial-trailing-zeroes/factorial-trailing-zeroes.cpp
+++ 172-factorial-trailing-zeroes/factorial-trailing-zeroes.cpp
@@ -2,8 +2,9 @@ class Solution {
 public:
     int trailingZeroes(int n) {
     int result = 0;
-    for(long long i=5; n/i>0; i*=5){
-        result += (n/i);
+    while(n>0){
+        result=result+(n/5);
+        n=n/5;
     }
     return result;
     }
