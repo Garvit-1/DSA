22c55238ff7d65549c6a7547898f9b6c6ad85569
Garvit-1 <117655352+Garvit-1@users.noreply.github.com>
2023-08-24 22:45:14 +0530

    Time: 18 ms (99.03%) | Memory: 32.6 MB (51.87%) - LeetSync

---
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        if (!root) return nullptr;
+
+        if (root->val == key) {
+            if (!root->left) return root->right;
+            if (!root->right) return root->left;
+            
+            TreeNode* minRight = findMin(root->right);
+            root->val = minRight->val;
+            root->right = deleteNode(root->right, minRight->val);
+        } else if (root->val > key) {
+            root->left = deleteNode(root->left, key);
+        } else {
+            root->right = deleteNode(root->right, key);
+        }
+
+        return root;
+    }
+
+    TreeNode* findMin(TreeNode* node) {
+        while (node->left) {
+            node = node->left;
+        }
+        return node;
+    }
+};
