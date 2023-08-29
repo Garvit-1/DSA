d43d2d21c151fd62bfd3e3c0f1f356ba96f766a0
Garvit-1 <117655352+Garvit-1@users.noreply.github.com>
2023-08-29 12:16:39 +0530

    Time: 24 ms (80.18%) | Memory: 10.7 MB (72.59%) - LeetSync

--- 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.cpp
+++ 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
     int bestClosingTime(string customers) {
-        int n=customers.size();
-        int cur=0;
-        int a=0,b=0;
-        for(int i=0;i<n;i++){
-            if(customers[i]=='Y'){
-                cur+=1;
-            }
-            else cur-=1;
-            cout<<cur<<" ";
-            if(cur>a){
-                a=cur;
-                b=i;
+        int count = 0;
+        int min = count,index = 0;
+        for(int i=1;i<=customers.length();i++){
+            if(customers[i-1]=='Y') count++;
+            else count--;
+            if(count>min){
+                min = count;
+                index = i;
             }
         }
-        return (b==0 && customers[0]=='N') ? b : b+1;
+        return index;
     }
 };
