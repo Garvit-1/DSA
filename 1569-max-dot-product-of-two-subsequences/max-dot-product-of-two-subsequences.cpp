8fb1d5e591028f2a7c48e6306858995e51ae3339
Garvit-1 <117655352+Garvit-1@users.noreply.github.com>
2023-10-08 22:02:53 +0530

    Time: 34 ms (55.78%) | Memory: 13.5 MB (26.99%) - LeetSync

--- 1569-max-dot-product-of-two-subsequences/max-dot-product-of-two-subsequences.cpp
+++ 1569-max-dot-product-of-two-subsequences/max-dot-product-of-two-subsequences.cpp
@@ -1,27 +1,29 @@
 class Solution {
 public:
+    vector<vector<int>> memo;
+
+    int help(vector<int>& nums1, vector<int>& nums2,int i,int j){
+    if(i==nums1.size() || j==nums2.size()) return 0;
+    if (memo[i][j] != INT_MIN)
+        return memo[i][j];
+
+    memo[i][j]= max(nums1[i]*nums2[j]+ help(nums1,nums2,i+1,j+1), max(help(nums1,nums2,i+1,j),help(nums1,nums2,i,j+1)) );
+
+    return memo[i][j];
+    }
     int maxDotProduct(vector<int>& nums1, vector<int>& nums2) {
-        int n = nums1.size();
-        int m = nums2.size();
-        vector<vector<int>> memo(n, vector<int>(m, INT_MIN));
-        
-        function<int(int, int)> dp = [&](int i, int j) {
-            if (i == n || j == m) {
-                return INT_MIN;
+        memo.assign(nums1.size(), vector<int>(nums2.size(), INT_MIN));
+
+        int ans=help(nums1,nums2,0,0);
+
+        if(ans==0) {
+            ans=INT_MIN;
+            for(auto a:nums1){
+                for(auto b:nums2) ans=max(a*b,ans);
             }
-            
-            if (memo[i][j] != INT_MIN) {
-                return memo[i][j];
-            }
-            
-            memo[i][j] = max(
-                nums1[i] * nums2[j] + max(dp(i + 1, j + 1), 0),
-                max(dp(i + 1, j), dp(i, j + 1))
-            );
-            
-            return memo[i][j];
-        };
-        
-        return dp(0, 0);
+        }
+
+        return ans;
+
     }
 };
