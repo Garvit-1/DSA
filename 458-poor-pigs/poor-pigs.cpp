37b275e8c378a5382848fb545288947be629f937
Garvit-1 <117655352+Garvit-1@users.noreply.github.com>
2023-10-29 12:39:17 +0530

    Time: 2 ms (53.09%) | Memory: 6.5 MB (39.09%) - LeetSync

--- 458-poor-pigs/poor-pigs.cpp
+++ 458-poor-pigs/poor-pigs.cpp
@@ -1,6 +1,12 @@
 class Solution {
 public:
-    int poorPigs(int buckets, int timeDetect, int timeTest) {
-        return ceil(log2(buckets)/log2(int(timeTest/timeDetect)+1));
+    int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
+        int rounds=minutesToTest/minutesToDie;
+
+        for(int i=0;i<buckets;i++){
+            if(pow((rounds+1),i) >= buckets) return i;
+        }
+
+        return -1;
     }
 };
