ea50657d65b8039bb2254a9d8e646e49fed1fd29
Garvit-1 <117655352+Garvit-1@users.noreply.github.com>
2023-11-22 19:38:02 +0530

    Time: 0 ms (100.00%) | Memory: 7.1 MB (78.20%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -1,13 +1,17 @@
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-        vector<vector<int>> v(numRows);
-        for(int i=0;i<numRows;i++){
-            v[i].resize(i+1,1);
+        vector<vector<int>> ans(numRows,vector<int>(1,1));
+        
+        for(int i=1;i<numRows;i++){
+            ans[i].resize(i+1,1);
             for(int j=1;j<i;j++){
-                v[i][j]=v[i-1][j]+v[i-1][j-1];
+                ans[i][j]=ans[i-1][j]+ans[i-1][j-1];
             }
         }
-        return v;
+
+        return ans;
+
+        
     }
 };
