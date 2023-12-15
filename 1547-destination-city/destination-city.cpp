dd5dd1be6c5201fc732cf68c98e6018efe94688a
Garvit-1 <117655352+Garvit-1@users.noreply.github.com>
2023-12-15 10:33:12 +0530

    Time: 6 ms (92.58%) | Memory: 10.8 MB (85.89%) - LeetSync

--- 1547-destination-city/destination-city.cpp
+++ 1547-destination-city/destination-city.cpp
@@ -1,13 +1,21 @@
 class Solution {
 public:
     string destCity(vector<vector<string>>& paths) {
-        map<string,int> mp;
-        for(auto a:paths){
-            mp[a[0]]++;
+        unordered_set<string> cities;
+
+        // Collect outgoing cities
+        for (const auto& path : paths) {
+            cities.insert(path[0]);
         }
-        for(auto a:paths){
-            if(mp[a[1]]==0) return a[1];
+
+        // Find destination city with no outgoing path
+        for (const auto& path : paths) {
+            const std::string& dest = path[1];
+            if (cities.find(dest) == cities.end()) {
+                return dest;
+            }
         }
+
         return "";
     }
 };
