c8024e15ab6da74e1d02dbf844c0bb633720dd32
Garvit-1 <117655352+Garvit-1@users.noreply.github.com>
2024-01-20 16:26:57 +0530

    Time: 76 ms (90.08%) | Memory: 52.7 MB (77.40%) - LeetSync

---
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool ispalin(const string& s, int i, int j) {
+        while (i <= j) {
+            if (s[i] != s[j]) return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    void help(vector<vector<string>>& ans, int l, vector<string>& v, const string& s) {
+        if (l >= s.length()) {
+            ans.push_back(v);
+            return;
+        }
+
+        for (int i = l; i < s.length(); i++) {
+            if (ispalin(s, l, i)) {
+                string currStr = s.substr(l, i - l + 1);
+                v.push_back(currStr);
+                help(ans, i + 1, v, s);
+                v.pop_back();
+            }
+        }
+    }
+
+    vector<vector<string>> partition(const string& s) {
+        vector<vector<string>> ans;
+        vector<string> v;
+        help(ans, 0, v, s);
+        return ans;
+    }
+};
