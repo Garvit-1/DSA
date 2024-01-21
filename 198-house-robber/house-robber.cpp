80f78cea610e7e994775b6df6281f3809ea5f606
Garvit-1 <117655352+Garvit-1@users.noreply.github.com>
2024-01-21 19:02:18 +0530

    Time: 4 ms (25.45%) | Memory: 9.1 MB (9.67%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
-    int help(vector<int>& nums,int i,vector<int> &dp){
-        if(i>=nums.size()){return 0;}
-        if(dp[i]!=-1) return dp[i];
-
-        return dp[i] = nums[i] + max(help(nums,i+2,dp),help(nums,i+3,dp));
-
-    }
     int rob(vector<int>& nums) {
+        if(nums.size()==1) return nums[0];
         vector<int> dp(nums.size(),-1);
-        return max(help(nums,0,dp),help(nums,1,dp));   
+        dp[0]=nums[0];
+        dp[1]=max(nums[0],nums[1]);
+        for(int i=2;i<nums.size();i++){
+            dp[i]=max(nums[i]+dp[i-2],dp[i-1]);
+        }
+        
+    return dp[nums.size()-1];
+        
     }
 };
