2fe4e8a1adf66f0aad10b440e0f5e2f685f755a0
Garvit-1 <117655352+Garvit-1@users.noreply.github.com>
2024-01-25 12:25:44 +0530

    Time: 72 ms (89.04%) | Memory: 49.2 MB (76.90%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -1,22 +1,61 @@
 class Solution {
 public:
-    int longestConsecutive(vector<int>& nums) { 
-        sort(nums.begin(), nums.end());
+    int longestConsecutive(vector<int>& nums) {
         int n = nums.size();
-        int maxi = 0;
-        int cnt = 1;
-        for(int i = 0; i < n; i++){
-            if(i < n-1 && nums[i] == nums[i+1]){
+        if(n==0) return 0;
+        sort(nums.begin(),nums.end());
+
+        int c=1;
+        int ans=0;
+        for(int i=1;i<n;i++){
+            if(nums[i]==nums[i-1]){
                 continue;
             }
-            if(i < n-1 && nums[i] + 1 == nums[i+1]){
-                cnt++;   
-            }
+            if(nums[i]==nums[i-1]+1) c++;
             else{
-                maxi = max(maxi, cnt);
-                cnt = 1;
-            }
+                ans=max(c,ans);c=1;
+                }
         }
-        return maxi;
+        ans=max(ans,c);
+        return ans;
+        
+    //     // Step 1: Handle the base case when the vector is empty.
+    //     if (n == 0) {
+    //         return 0;
+    //     }
+
+    //     unordered_set<int> s;   // Create an unordered set to store unique elements.
+    //     int cnt = 1;            // Initialize a counter for the current consecutive sequence length.
+    //     int longest = 1;        // Initialize a variable to store the maximum consecutive sequence length.
+
+    //     // Step 2: Insert all elements of 'nums' into the unordered set 's'.
+    //     for (int i = 0; i < n; i++) {
+    //         s.insert(nums[i]);
+    //     }
+        
+    //     int x = 0;  // Initialize a variable to keep track of the current element being processed.
+
+    //     // Step 3: Iterate through the elements of the unordered set.
+    //     for (auto a : s) {
+    //         // Step 4: If the current element 'a' is the start of a sequence (no 'a-1' in 's'),
+    //         if (s.find(a - 1) == s.end()) {
+    //             cnt = 1;  // Reset the counter.
+    //             x = a;    // Update 'x' to the current element 'a'.
+
+    //             // Step 5: While consecutive elements exist in 's', increment 'cnt' and 'x'.
+    //             while (s.find(x + 1) != s.end()) {
+    //                 cnt++;
+    //                 x = x + 1;
+    //             }
+    //         }
+            
+    //         // Step 6: Update 'longest' with the maximum of 'longest' and 'cnt'.
+    //         longest = max(longest, cnt);
+    //     }
+
+    //     // Step 7: Return 'longest' as the result, representing the length of the longest consecutive sequence.
+    //     return longest;
+    
+    
     }
 };
