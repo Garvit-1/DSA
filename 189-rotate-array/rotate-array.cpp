d5a2cbab3d3ff33b68ce9438aeaba43afc709eb0
Garvit-1 <117655352+Garvit-1@users.noreply.github.com>
2024-01-27 15:41:57 +0530

    Time: 25 ms (40.88%) | Memory: 27.3 MB (7.89%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -1,11 +1,19 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
+
         int n=nums.size();
+        if(n==1) return;
         k=k%n;
-        reverse(nums.begin(),nums.end());
-        reverse(nums.begin(),nums.begin()+k);
-        reverse(nums.begin()+k,nums.end());
+        int a=n-k-1;
+
+        int i=0;int j=a;
+        while(i<j) swap(nums[i++],nums[j--]);
+        i=a+1;j=n-1;
+        while(i<j) swap(nums[i++],nums[j--]);
+
+        i=0;j=n-1;
+        while(i<j) swap(nums[i++],nums[j--]);
         
     }
 };
