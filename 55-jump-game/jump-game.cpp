7df3bfa282f303020aa7585f78dd923b64318a18
Garvit-1 <117655352+Garvit-1@users.noreply.github.com>
2024-01-27 16:25:11 +0530

    Time: 638 ms (12.32%) | Memory: 53.9 MB (5.79%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
-    bool canJump(vector<int>& nums) {
-        
-        int maxi=0;
-        for(int i=0;i<nums.size();i++){
-            if (i > maxi) {
-                // If the current index is unreachable, return false
-                return false;
-            }
-            maxi=max(nums[i]+i,maxi);
-        if(maxi>=nums.size()-1) return true;
+    int help(vector<int> &nums,int i,vector<int> &dp){
+        if(i==nums.size()-1) return true;
+        if(dp[i]!=-1) return dp[i];
+        bool ans=0;
+        for(int x=1;x<=nums[i];x++){
+            ans = ans || help(nums,i+x,dp);
+            if(ans) return dp[i]=ans;
         }
-return false;
+        return dp[i]=0;
+    }
+    bool canJump(vector<int>& nums) {
+        vector<int> dp(nums.size(),-1);
+        return help(nums,0,dp);
+        
     }
 };
