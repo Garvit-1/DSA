4ea32c0e25ad4a46c8560e6a4f80d0705b115de2
Garvit-1 <117655352+Garvit-1@users.noreply.github.com>
2024-01-27 16:27:30 +0530

    Time: 44 ms (78.73%) | Memory: 52.9 MB (5.79%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -1,18 +1,12 @@
 class Solution {
-public:
-    int help(vector<int> &nums,int i,vector<int> &dp){
-        if(i==nums.size()-1) return true;
-        if(dp[i]!=-1) return dp[i];
-        bool ans=0;
-        for(int x=1;x<=nums[i];x++){
-            ans = ans || help(nums,i+x,dp);
-            if(ans) return dp[i]=ans;
+    public:
+    bool canJump(vector<int> nums) {
+        int destination=nums.size()-1; 
+        for(int i=nums.size()-2;i>=0;i--){ 
+            if(i+nums[i]>=destination){  
+                destination=i;
+            }
         }
-        return dp[i]=0;
-    }
-    bool canJump(vector<int>& nums) {
-        vector<int> dp(nums.size(),-1);
-        return help(nums,0,dp);
-        
+        return destination==0; 
     }
 };
