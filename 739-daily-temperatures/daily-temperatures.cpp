e703b234875ea0192b911895d5458eaf6f573e99
Garvit-1 <117655352+Garvit-1@users.noreply.github.com>
2024-01-31 11:52:11 +0530

    Time: 104 ms (98.35%) | Memory: 101.1 MB (64.78%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.cpp
+++ 739-daily-temperatures/daily-temperatures.cpp
@@ -1,22 +1,88 @@
 class Solution {
 public:
-    vector<int> dailyTemperatures(vector<int>& temperatures) {
-        stack<pair<int, int>> stk;
-        int n = temperatures.size();
-        for(int i=n-1; i>=0; i--){
-            int val = 1;
-            int element = temperatures[i];
-            if(stk.empty()) temperatures[i] = 0;
+    vector<int> dailyTemperatures(vector<int>& temp) {
+        int n=temp.size();
+        vector<int> ans(n,0);
+        stack<int> st;
+        for(int i=n-1;i>=0;i--){
+            if(st.empty()){
+                st.push(i);
+                ans[i]=0;
+            }
             else{
-                while(!stk.empty() && stk.top().first <= temperatures[i]){
-                    val+= stk.top().second;
-                    stk.pop();
+                while(!st.empty() && temp[st.top()]<=temp[i])
+                {
+                    st.pop();
                 }
-                (stk.empty()) ? temperatures[i] = 0 : temperatures[i] = val;
-            }  
-            stk.push({element, val});
-        }
 
-        return temperatures;
+                if(st.empty()){
+                    ans[i]=0;
+                }
+                else ans[i]=st.top()-i;
+                st.push(i);
+            }
+        }
+        return ans;
+        
     }
 };
+
+// class Solution {
+// public:
+//     vector<int> dailyTemperatures( vector<int>& temperatures) {
+//         deque<int> deque;
+        
+//         vector<int> res(temperatures.size(), 0);
+
+//         for (int i = temperatures.size() - 1; i >= 0; --i) {
+//             if (deque.empty()) {
+//                 deque.push_front(i);
+//                 res[i] = 0;
+//             } else {
+//                 while (!deque.empty() && temperatures[i] >= temperatures[deque.front()]) {
+//                     deque.pop_front();
+//                 }
+
+//                 if (deque.empty()) {
+//                     res[i] = 0;
+//                 } else {
+//                     res[i] = deque.front() - i;
+//                 }
+
+//                 deque.push_front(i);
+//             }
+//         }
+
+//         return res;
+//     }
+// }; 
+
+
+
+
+
+
+// class Solution {
+// public:
+//     vector<int> dailyTemperatures(vector<int>& temperatures) {
+//         vector<int> result(temperatures.size(), 0);
+//         vector<int> stack;
+
+//         for (int i = temperatures.size() - 1; i >= 0; --i) {
+//             int currTemperature = temperatures[i];
+
+//             while (!stack.empty() && currTemperature >= temperatures[stack.back()]) {
+//                 stack.pop_back();
+//             }
+
+//             if (!stack.empty()) {
+//                 result[i] = stack.back() - i;
+//             }
+
+//             stack.push_back(i);
+//         }
+
+//         return result;
+//     }
+// };
+
