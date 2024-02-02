834e5bd5d81065252a5488b88cc45611c55effa7
Garvit-1 <117655352+Garvit-1@users.noreply.github.com>
2024-02-02 15:29:02 +0530

    Time: 13 ms (60.98%) | Memory: 17.6 MB (25.89%) - LeetSync

--- 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.cpp
+++ 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.cpp
@@ -1,45 +1,32 @@
-/*
-// Definition for a Node.
-class Node {
-public:
-    int val;
-    Node* left;
-    Node* right;
-    Node* next;
-
-    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
-
-    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
-
-    Node(int _val, Node* _left, Node* _right, Node* _next)
-        : val(_val), left(_left), right(_right), next(_next) {}
-};
-*/
+#include <queue>
 
 class Solution {
 public:
     Node* connect(Node* root) {
-        if(!root) return root;
-        queue<Node*> q;
+        if (!root) return root;
+        
+        std::queue<Node*> q;
         q.push(root);
-        while(!q.empty()){
-            int n=q.size();
-            vector<Node*> temp;
-            for(int i=0;i<n;i++){
-                Node* cur=q.front();
+        
+        while (!q.empty()) {
+            int n = q.size();
+            Node* right = nullptr;
+            
+            for (int i = 0; i < n; i++) {
+                Node* cur = q.front();
                 q.pop();
-                temp.push_back(cur);
-                if(cur->left!=NULL) q.push(cur->left);
-                if(cur->right!=NULL) q.push(cur->right);
+                cur->next = right;
+                right = cur;
+
+                if (cur->right) {
+                    q.push(cur->right);
+                }
+                if (cur->left) {
+                    q.push(cur->left);
+                }
             }
-            int i=0;
-            for(i=0;i<n-1;i++){
-                temp[i]->next=temp[i+1];
-            }
-            temp[i]->next=NULL;
-            temp.clear();
         }
         
-    return root;
+        return root;
     }
 };
