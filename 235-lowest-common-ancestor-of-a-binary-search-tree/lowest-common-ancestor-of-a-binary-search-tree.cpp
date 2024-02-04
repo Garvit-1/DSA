d2d7b559ae57e1ab74297114b1e0aebf509cb7f3
Garvit-1 <117655352+Garvit-1@users.noreply.github.com>
2024-02-04 19:42:43 +0530

    Time: 28 ms (32.00%) | Memory: 21.8 MB (80.69%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -6,42 +6,21 @@
  *     TreeNode *right;
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
- **/
+ */
 
 class Solution {
 public:
-    vector<TreeNode*> a, b;
-
-    void help(TreeNode* root, TreeNode* p, TreeNode* q, vector<TreeNode*>& curr) {
-        if (!root) return;
-
-        if (root == p) {
-            a = curr;
-            a.push_back(p);
+ TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        int small = min(p->val, q->val);
+        int large = max(p->val, q->val);
+        while (root != nullptr) {
+            if (root->val > large) // p, q belong to the left subtree
+                root = root->left;
+            else if (root->val < small) // p, q belong to the right subtree
+                root = root->right;
+            else // Now, small <= root.val <= large -> This root is the LCA between p and q
+                return root;
         }
-
-        if (root == q) {
-            b = curr;
-            b.push_back(q);
-        }
-
-        curr.push_back(root);
-        help(root->left, p, q, curr);
-        help(root->right, p, q, curr);
-        curr.pop_back();
-    }
-
-    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        vector<TreeNode*> v;
-        help(root, p, q, v);
-
-        int n = min(a.size(), b.size());
-        for (int i = n - 1; i >= 0; i--) {
-            if (a[i] == b[i]) {
-                return a[i];
-            }
-        }
-
         return nullptr;
     }
 };
