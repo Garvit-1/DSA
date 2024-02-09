c265037fc7b6128b0d5f7ae46c58dba5956eb10b
Garvit-1 <117655352+Garvit-1@users.noreply.github.com>
2024-02-09 23:50:59 +0530

    Time: 7 ms (84.18%) | Memory: 13.5 MB (32.65%) - LeetSync

--- 6-zigzag-conversion/zigzag-conversion.cpp
+++ 6-zigzag-conversion/zigzag-conversion.cpp
@@ -1,24 +1,30 @@
 class Solution {
 public:
-    string convert(string s, int numRows) {
-        vector<string> v(numRows);
-        if(numRows<=1) return s;
-        int i=0,j=0;
-        bool flag=true;
-        while(i<s.length()){
-            string x=v[j];
-            x+=s[i];
-            v[j]=x;
-            i++;
-            if(flag){
-                j++;
+    string convert(string s, int n) {
+        vector<string> v(n,"");
+        int i=0;bool flag=true;
+        if(n==1) return s;
+        for(auto a:s){
+            v[i]+=a;
+            if(flag) {
+                if(i==n-1){
+                    flag=!flag;
+                    i--;
+                }
+                else i++;
             }
-            else j--;
-            if(j==numRows-1 || j==0) flag=!flag;
-            
+            else{ 
+                if(i==0){
+                flag=!flag;
+                i++;
+                }
+                else i--;
+            }
         }
         string ans="";
         for(auto a:v) ans+=a;
         return ans;
+        
+        
     }
 };
