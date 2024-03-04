ea1ce0f68471412339d29c674aab6f21a11543d9
Garvit-1 <117655352+Garvit-1@users.noreply.github.com>
2024-03-05 00:39:21 +0530

    Time: 8 ms (18.89%) | Memory: 10.6 MB (9.55%) - LeetSync

--- 3055-maximum-odd-binary-number/maximum-odd-binary-number.cpp
+++ 3055-maximum-odd-binary-number/maximum-odd-binary-number.cpp
@@ -1,20 +1,10 @@
 class Solution {
 public:
     string maximumOddBinaryNumber(string s) {
-        int a,b=s.length();
-        a=0;
-        for(int i=0;i<b;i++){
-            if(s[i]=='1') a++;
-            s[i]='0';
+        sort(s.rbegin(),s.rend());
+        for(int i = s.size()-1;i>=0;i--){
+            if(s[i]=='1')swap(s[i],s[s.size()-1]);
         }
-        s[b-1]='1';
-        for(int i=0;i<a-1;i++){
-            s[i]='1';
-        }
-
         return s;
-        
-
-        
     }
 };
